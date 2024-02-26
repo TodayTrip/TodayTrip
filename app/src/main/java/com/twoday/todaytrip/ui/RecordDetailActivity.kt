30b9815a9899061b26aae3dc3eef87f4baf38866
sunjoolee <sheisthepoem@naver.com>
2024-02-26 15:38:22 +0900

    [Refactor] Add RecordActivity, Delete RecordFragment

---
+++ app/src/main/java/com/twoday/todaytrip/ui/RecordDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.twoday.todaytrip.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.twoday.todaytrip.R
+
+class RecordDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_detail)
+    }
+}
