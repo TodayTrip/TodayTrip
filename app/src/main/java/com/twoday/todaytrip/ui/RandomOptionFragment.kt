d9792c48749e0d28a9c2b85d50f419da5c29dcb2
nhkim <nhkim1529@gmail.com>
2024-02-27 20:45:16 +0900

    [Feat] RandomOptionFragment 버튼 클릭 코드 추가

--- app/src/main/java/com/twoday/todaytrip/ui/RandomOptionFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/RandomOptionFragment.kt
@@ -1,60 +1,66 @@
 package com.twoday.todaytrip.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.twoday.todaytrip.R
+import com.twoday.todaytrip.databinding.FragmentRandomOptionBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RandomOptionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RandomOptionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentRandomOptionBinding? = null
+    private val binding get() = _binding!!
+    private var isSelectedAllRandomBtn = false
+    private var isSelectedThemeRandomBtn = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_random_option, container, false)
+        _binding = FragmentRandomOptionBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RandomOptionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RandomOptionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnAllRandom.setBackgroundResource(R.drawable.shape_yellow_boarder)
+        setUpClickListener()
+    }
+
+    private fun setUpClickListener() {
+        binding.btnOptionSelect.setOnClickListener{
+            Log.d("btn", "올랜덤 $isSelectedAllRandomBtn, 테마랜덤 $isSelectedThemeRandomBtn")
+            findNavController().navigate(R.id.action_navigation_random_option_to_navigation_random_theme)
+        }
+        binding.btnAllRandom.setOnClickListener {
+            selectButton(isAllRandom = true)
+
+        }
+        binding.btnThemeRandom.setOnClickListener {
+            selectButton(isAllRandom = false)
+        }
+    }
+
+    private fun selectButton(isAllRandom: Boolean) {
+        isSelectedAllRandomBtn = isAllRandom
+        isSelectedThemeRandomBtn = !isAllRandom
+
+        binding.btnAllRandom.apply {
+            setBackgroundResource(if (isAllRandom) R.drawable.shape_yellow_boarder else R.drawable.shape_without_boarder)
+        }
+
+        binding.btnThemeRandom.apply {
+            setBackgroundResource(if (!isAllRandom) R.drawable.shape_yellow_boarder else R.drawable.shape_without_boarder)
+        }
+
+        Log.d("btn", "올랜덤 $isSelectedAllRandomBtn, 테마랜덤 $isSelectedThemeRandomBtn")
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
