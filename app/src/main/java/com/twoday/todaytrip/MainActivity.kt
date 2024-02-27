90cdab2657f42769d6640e115581876e8c594a6b
nhkim <nhkim1529@gmail.com>
2024-02-27 20:48:10 +0900

    [Feat] MainActivity에 navController 추가

--- app/src/main/java/com/twoday/todaytrip/MainActivity.kt
+++ app/src/main/java/com/twoday/todaytrip/MainActivity.kt
@@ -2,10 +2,22 @@ package com.twoday.todaytrip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.twoday.todaytrip.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHomeFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_main_fragment) as NavHostFragment
+        val navController = navHomeFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
