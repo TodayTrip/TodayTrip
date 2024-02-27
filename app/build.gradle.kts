5695d9e921b41433328ebe52cae75e467488fedd
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-02-27 21:00:59 +0900

    [Rename] xml 항목 id rename

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,9 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     //RecordFragment Charts
     implementation ("com.diogobernardino:williamchart:3.10.1")
-    //Naver Map
+    //Map
     implementation("com.naver.maps:map-sdk:3.17.0")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
     //Retrofit
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
