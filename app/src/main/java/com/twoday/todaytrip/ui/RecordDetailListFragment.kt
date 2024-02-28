4f6f5d2a7949efc47f2aeb36a21ffcc3abec408f
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-02-28 21:18:27 +0900

    [Feat] 기록 디테일 list 페이지 프래그먼트에 지도 추가

--- app/src/main/java/com/twoday/todaytrip/ui/RecordDetailListFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/RecordDetailListFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 import com.twoday.todaytrip.R
+import com.twoday.todaytrip.databinding.FragmentRecordDetailListBinding
+import com.twoday.todaytrip.databinding.FragmentRecordDetailMapBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,13 +23,20 @@ private const val ARG_PARAM2 = "param2"
  * Use the [RecordDetailListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class RecordDetailListFragment : Fragment() {
+class RecordDetailListFragment : Fragment(), OnMapReadyCallback {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentRecordDetailListBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var map: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = FragmentRecordDetailListBinding.inflate(layoutInflater)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -35,7 +48,15 @@ class RecordDetailListFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record_detail_list, container, false)
+//        return inflater.inflate(R.layout.fragment_record_detail_list, container, false)
+        return binding.root
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.map = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        naverMap.locationTrackingMode = LocationTrackingMode.Face
     }
 
     companion object {
