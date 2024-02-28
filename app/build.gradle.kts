424177fdd5a4f2f56132530e33d1e193b1ef9bde
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-02-28 11:05:36 +0900

    [Setting] version sync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.airbnb.android:lottie:6.3.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     //ViewModel
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
@@ -83,4 +82,6 @@ dependencies {
     //Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    //Lottie
+    implementation("com.airbnb.android:lottie:6.3.0")
 }
