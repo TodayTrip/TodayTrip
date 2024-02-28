5628c6c92f4f87a768dbd41d50f6ec14449dd4fc
nhkim <nhkim1529@gmail.com>
2024-02-28 15:27:59 +0900

    [Feat] 상수 파일 Constans 추가

---
+++ app/src/main/java/com/twoday/todaytrip/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.twoday.todaytrip.utils
+
+object Constants {
+    const val PREFERENCE_DESTINATION_KEY = "PREFERENCE_DESTINATION_KEY"
+    const val DESTINATION_KEY = "DESTINATION_KEY"
+}
