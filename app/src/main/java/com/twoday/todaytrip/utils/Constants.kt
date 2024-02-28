217f7e64da20215e01dabc816ba38e3bd9369eb1
nhkim <nhkim1529@gmail.com>
2024-02-28 20:27:19 +0900

    [Setting] THEME_KEY 상수 추가

--- app/src/main/java/com/twoday/todaytrip/utils/Constants.kt
+++ app/src/main/java/com/twoday/todaytrip/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.twoday.todaytrip.utils
 
 object Constants {
-    const val PREFERENCE_DESTINATION_KEY = "PREFERENCE_DESTINATION_KEY"
-    const val DESTINATION_KEY = "DESTINATION_KEY"
+    const val PREFERENCE_DESTINATION_KEY = "PREFERENCE_DESTINATION_KEY" // preference 주소
+    const val DESTINATION_KEY = "DESTINATION_KEY" // 지역
+    const val THEME_KEY = "THEME_KEY" // 테마
 }
