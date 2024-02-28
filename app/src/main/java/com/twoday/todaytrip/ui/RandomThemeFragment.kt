ee9a6f5fdbe5643454e76701a9b2f2d1d30288c9
nhkim <nhkim1529@gmail.com>
2024-02-28 20:37:22 +0900

    [Feat] 다음버튼을 눌렀을 때만 버튼 전달

--- app/src/main/java/com/twoday/todaytrip/ui/RandomThemeFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/RandomThemeFragment.kt
@@ -8,57 +8,52 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.twoday.todaytrip.R
-import com.twoday.todaytrip.databinding.FragmentRandomOptionBinding
 import com.twoday.todaytrip.databinding.FragmentRandomThemeBinding
+import com.twoday.todaytrip.utils.Constants
+import com.twoday.todaytrip.utils.DestinationData
+import com.twoday.todaytrip.utils.SharedPreferencesUtil
 
 class RandomThemeFragment : Fragment() {
     private var _binding: FragmentRandomThemeBinding? = null
     private val binding get() = _binding!!
-    private var isBtnSelected = false
+    private var selectedTheme: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentRandomThemeBinding.inflate(inflater, container, false)
-        binding.btnTripTheme.isEnabled = false
-        setUpClickListener()
+        binding.btnNext.isEnabled = false
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpClickListener()
     }
 
     private fun setUpClickListener() {
-        binding.btnTripTheme.setOnClickListener{
-            findNavController().navigate(R.id.action_navigation_random_theme_to_navigation_random_result)
+        binding.btnNext.setOnClickListener {
+            selectedTheme?.let {
+                val action = RandomThemeFragmentDirections.actionNavigationRandomThemeToNavigationRandomResult()
+                findNavController().navigate(action)
+            }
         }
-        binding.btnTheme1.setOnClickListener {
-            selectButton(selectedNumber = 0)
-        }
-        binding.btnTheme2.setOnClickListener {
-            selectButton(selectedNumber = 1)
-        }
-        binding.btnTheme3.setOnClickListener {
-            selectButton(selectedNumber = 2)
-        }
-        binding.btnTheme4.setOnClickListener {
-            selectButton(selectedNumber = 3)
-        }
-        binding.btnTheme5.setOnClickListener {
-            selectButton(selectedNumber = 4)
-        }
-        binding.btnTheme6.setOnClickListener {
-            selectButton(selectedNumber = 5)
-        }
-        binding.btnTheme7.setOnClickListener {
-            selectButton(selectedNumber = 6)
-        }
+        binding.btnTheme1.setOnClickListener { selectTheme("산", 0) }
+        binding.btnTheme2.setOnClickListener { selectTheme("바다", 1) }
+        binding.btnTheme3.setOnClickListener { selectTheme("역사", 2) }
+        binding.btnTheme4.setOnClickListener { selectTheme("휴양", 3) }
+        binding.btnTheme5.setOnClickListener { selectTheme("체험", 4) }
+        binding.btnTheme6.setOnClickListener { selectTheme("레포츠", 5) }
+        binding.btnTheme7.setOnClickListener { selectTheme("문화시설", 6) }
     }
 
+    private fun selectTheme(theme: String, selectedNumber: Int) {
+        selectedTheme = theme
+        selectButton(selectedNumber)
+        selectRandomDestination(theme)
+    }
     private fun selectButton(selectedNumber: Int) {
-        // 모든 버튼의 선택 상태를 초기화
         val buttons = listOf(
             binding.btnTheme1,
             binding.btnTheme2,
@@ -68,7 +63,6 @@ class RandomThemeFragment : Fragment() {
             binding.btnTheme6,
             binding.btnTheme7
         )
-
         // 모든 버튼을 순회하면서, 선택된 번호의 버튼에만 특정 스타일 적용
         for ((index, button) in buttons.withIndex()) {
             if (index == selectedNumber) {
@@ -77,11 +71,17 @@ class RandomThemeFragment : Fragment() {
                 button.setBackgroundResource(R.drawable.shape_without_boarder)
             }
         }
+        binding.btnNext.isEnabled = true
+    }
 
-        // '다음' 버튼 활성화
-        binding.btnTripTheme.isEnabled = true
-
-        Log.d("btn", "선택된 테마는 ${selectedNumber}번째 테마")
+    private fun selectRandomDestination(theme: String) {
+        val themeDestination = DestinationData.themeDestinations[theme]?.random()
+        Log.d("themeDestination", themeDestination.toString())
+        themeDestination?.let {
+            Log.d("themeDestination", it)
+            SharedPreferencesUtil.saveDestination(requireContext(), it, Constants.DESTINATION_KEY)
+            SharedPreferencesUtil.saveDestination(requireContext(), theme, Constants.THEME_KEY)
+        }
     }
 
     override fun onDestroyView() {
