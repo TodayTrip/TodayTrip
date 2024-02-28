80525fa69cf07c75bfec502afe458f74e89a7867
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-02-28 21:09:29 +0900

    [Feat] 기록 디테일 페이지 프래그먼트에 지도 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
 }
