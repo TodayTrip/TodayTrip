9662b03e6d2154d48b890a70d747b7cf12dc956f
sunjoolee <sheisthepoem@naver.com>
2024-03-01 00:12:34 +0900

    [Rename] Rename Constants to PrefConstants
    Add tour info list keys

---
+++ app/src/main/java/com/twoday/todaytrip/utils/PrefConstants.kt
@@ -0,0 +1,12 @@
+package com.twoday.todaytrip.utils
+
+object PrefConstants {
+    const val PREFERENCE_DESTINATION_KEY = "PREFERENCE_DESTINATION_KEY" // preference 주소
+    const val DESTINATION_KEY = "DESTINATION_KEY" // 지역
+    const val THEME_KEY = "THEME_KEY" // 테마
+
+    const val TOUR_INFO_TAB_LIST = "TOUR_INFO_TAB_LIST" // 관광지 탭 정보 목록
+    const val RESTAURANT_TAB_LIST = "RESTAURANT_TAB_LIST" // 음식점 탭 정보 목록
+    const val CAFE_TAB_LIST = "CAFE_TAB_LIST" // 카페 탭 정보 목록
+    const val EVENT_TAB_LIST = "EVENT_TAB_LIST" // 행사/공연/축제 탭 정보 목록
+}
