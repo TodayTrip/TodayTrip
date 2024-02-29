2e54470eca0713a39ba43b5346bc951f36f95ea0
sunjoolee <sheisthepoem@naver.com>
2024-03-01 01:03:06 +0900

    [Feat] Add tour category1 id enum class

---
+++ app/src/main/java/com/twoday/todaytrip/tourData/TourCategoryId.kt
@@ -0,0 +1,17 @@
+package com.twoday.todaytrip.tourData
+
+enum class TourCategoryId1(val id:String){
+    NATURE("A01"),
+    HUMANITIES("A02"),
+    LEISURE_SPORTS("A03"),
+    SHOPPING("A04"),
+    FOOD("A05"),
+    LODGEMENT("B02"),
+    TOUR_COURSE("C01")
+}
+enum class TourCategoryId2(val id:String){
+    // TODO 카테고리 중분류 enum class
+}
+enum class TourCategoryId3(val id:String){
+    // TODO 카테고리 소분류 enum class
+}
