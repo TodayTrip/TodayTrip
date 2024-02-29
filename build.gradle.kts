3eb242c873bd2a6b3335127e1750e4523150d84d
nhkim <nhkim1529@gmail.com>
2024-02-29 21:58:34 +0900

    [Setting] safeargs, 파이어베이스 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
