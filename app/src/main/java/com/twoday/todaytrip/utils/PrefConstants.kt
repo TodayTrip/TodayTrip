1b4e72c4f40620720549e1cb17a3cd1cbc65b323
sunjoolee <sheisthepoem@naver.com>
2024-03-01 00:16:43 +0900

    [Rename] Rename keys in PrefConstants

--- app/src/main/java/com/twoday/todaytrip/utils/PrefConstants.kt
+++ app/src/main/java/com/twoday/todaytrip/utils/PrefConstants.kt
@@ -5,8 +5,8 @@ object PrefConstants {
     const val DESTINATION_KEY = "DESTINATION_KEY" // 지역
     const val THEME_KEY = "THEME_KEY" // 테마
 
-    const val TOUR_INFO_TAB_LIST = "TOUR_INFO_TAB_LIST" // 관광지 탭 정보 목록
-    const val RESTAURANT_TAB_LIST = "RESTAURANT_TAB_LIST" // 음식점 탭 정보 목록
-    const val CAFE_TAB_LIST = "CAFE_TAB_LIST" // 카페 탭 정보 목록
-    const val EVENT_TAB_LIST = "EVENT_TAB_LIST" // 행사/공연/축제 탭 정보 목록
+    const val TOUR_INFO_TAB_LIST_KEY = "TOUR_INFO_TAB_LIST_KEY" // 관광지 탭 정보 목록
+    const val RESTAURANT_TAB_LIST_KEY = "RESTAURANT_TAB_LIST_KEY" // 음식점 탭 정보 목록
+    const val CAFE_TAB_LIST_KEY = "CAFE_TAB_LIST_KEY" // 카페 탭 정보 목록
+    const val EVENT_TAB_LIST_KEY = "EVENT_TAB_LIST_KEY" // 행사/공연/축제 탭 정보 목록
 }
