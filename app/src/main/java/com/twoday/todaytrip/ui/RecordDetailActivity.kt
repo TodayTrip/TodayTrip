0af3c9c2bcefd3d01249e242e317144625e738b9
nhkim <nhkim1529@gmail.com>
2024-03-01 20:27:59 +0900

    [Rename] 파일 저장 위치 변경

--- app/src/main/java/com/twoday/todaytrip/ui/RecordDetailActivity.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/RecordDetailActivity.kt
@@ -6,13 +6,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.util.FusedLocationSource
 import com.twoday.todaytrip.R
 import com.twoday.todaytrip.databinding.ActivityRecordDetailBinding
+import com.twoday.todaytrip.ui.record.RecordDetailListFragment
+import com.twoday.todaytrip.ui.record.RecordDetailMapFragment
 import com.twoday.todaytrip.viewModel.RecordDetailViewModel
 
 class RecordDetailActivity : AppCompatActivity() {
