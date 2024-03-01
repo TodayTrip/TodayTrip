3814fd860acebffa85fd931458db77591111fe5e
sunjoolee <sheisthepoem@naver.com>
2024-03-01 16:24:02 +0900

    [Feat] Add Offline Cache Interceptor & Cache Interceptor

--- app/src/main/java/com/twoday/todaytrip/tourApi/TourNetworkClient.kt
+++ app/src/main/java/com/twoday/todaytrip/tourApi/TourNetworkClient.kt
@@ -1,29 +1,74 @@
 package com.twoday.todaytrip.tourApi
 
+import android.util.Log
+import com.google.common.net.HttpHeaders.CACHE_CONTROL
 import com.twoday.todaytrip.BuildConfig
+import com.twoday.todaytrip.MyApplication
+import com.twoday.todaytrip.utils.NetworkUtil
+import okhttp3.Cache
+import okhttp3.CacheControl
+import okhttp3.Interceptor
+import okhttp3.Interceptor.*
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
+
 
 object TourNetworkClient {
     private const val TOUR_BASE_URL = "https://apis.data.go.kr/B551011/KorService1/"
 
-    private fun createOkHttpClient(): OkHttpClient {
-        val interceptor = HttpLoggingInterceptor()
-
-        if (BuildConfig.DEBUG)
-            interceptor.level = HttpLoggingInterceptor.Level.BODY
-        else
-            interceptor.level = HttpLoggingInterceptor.Level.NONE
-
-        return OkHttpClient.Builder()
-            .connectTimeout(20, TimeUnit.SECONDS)
+    private fun createOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(50, TimeUnit.SECONDS)
             .readTimeout(20, TimeUnit.SECONDS)
             .writeTimeout(20, TimeUnit.SECONDS)
-            .addNetworkInterceptor(interceptor)
+            .addInterceptor(provideOfflineCacheInterceptor())
+            .addNetworkInterceptor(provideCacheInterceptor())
+            .cache(provideCache())
             .build()
+
+    private fun provideCache(): Cache? {
+        var cache: Cache? = null
+        try {
+            cache = Cache(
+                File(MyApplication.appContext!!.cacheDir, "http-cache"),
+                10 * 1024 * 1024
+            ) // 10 MB
+        } catch (e: Exception) {
+            Log.d("TourNetworkClient", "Could not create Cache!")
+        }
+        return cache
+    }
+    private fun provideCacheInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val response: Response = chain.proceed(chain.request())
+
+            // re-write response header to force use of cache
+            val cacheControl: CacheControl = CacheControl.Builder()
+                .maxAge(2, TimeUnit.MINUTES)
+                .build()
+            response.newBuilder()
+                .header(CACHE_CONTROL, cacheControl.toString())
+                .build()
+        }
+    }
+    private fun provideOfflineCacheInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            var request = chain.request()
+            if (!NetworkUtil.isNetworkAvailable()) {
+                val cacheControl: CacheControl = CacheControl.Builder()
+                    .maxStale(7, TimeUnit.DAYS)
+                    .build()
+                request = request.newBuilder()
+                    .cacheControl(cacheControl)
+                    .build()
+            }
+            chain.proceed(request)
+        }
     }
 
     private val tourRetrofit = Retrofit.Builder()
@@ -31,5 +76,6 @@ object TourNetworkClient {
             createOkHttpClient()
         ).build()
 
-    val tourNetWork: TourNetworkInterface = tourRetrofit.create(TourNetworkInterface::class.java)
+    val tourNetWork: TourNetworkInterface =
+        tourRetrofit.create(TourNetworkInterface::class.java)
 }
