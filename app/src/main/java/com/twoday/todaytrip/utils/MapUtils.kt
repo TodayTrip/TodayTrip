94e4f35e22cdcc286034e270ee59e908afa16e32
nhkim <nhkim1529@gmail.com>
2024-03-04 09:57:43 +0900

    [Feat] MapUtils에 combineImages 추가

--- app/src/main/java/com/twoday/todaytrip/utils/MapUtils.kt
+++ app/src/main/java/com/twoday/todaytrip/utils/MapUtils.kt
@@ -3,6 +3,8 @@ package com.twoday.todaytrip.utils
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Matrix
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.CameraUpdate
@@ -11,14 +13,6 @@ import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.PolylineOverlay
 
 object MapUtils {
-    // 폴리라인 그리는 함수
-    fun drawPolyline(naverMap: NaverMap, sequence: List<LatLng>) {
-        PolylineOverlay().apply {
-            coords = sequence
-            color = 0xFF0085FF.toInt()
-            map = naverMap
-        }
-    }
     // 마커 아이콘 크기 조절하는 함수
     fun resizeMapIcons(context: Context, iconId: Int, width: Int, height: Int): Bitmap {
         val imageBitmap = BitmapFactory.decodeResource(context.resources, iconId)
@@ -38,5 +32,25 @@ object MapUtils {
         val cameraUpdate = CameraUpdate.fitBounds(bounds, padding)
         naverMap.moveCamera(cameraUpdate)
     }
+    // 마커 이미지와 일반 이미지를 합치는 함수
+    fun combineImages(markerIcon: Bitmap, photo: Bitmap): Bitmap {
+        val combinedImage = Bitmap.createBitmap(markerIcon.width, markerIcon.height, markerIcon.config)
+        val canvas = Canvas(combinedImage)
 
+        canvas.drawBitmap(markerIcon, Matrix(), null)
+
+        val left = (markerIcon.width - photo.width) / 2f // 가로 중앙 위치 계산
+        val top = (markerIcon.height / 7f) - (photo.height / 8f)
+        canvas.drawBitmap(photo, left, top, null) // 조정된 위치에 사진을 그립니다.
+
+        return combinedImage
+    }
+    // 폴리라인 그리는 함수
+    fun drawPolyline(naverMap: NaverMap, sequence: List<LatLng>) {
+        PolylineOverlay().apply {
+            coords = sequence
+            color = 0xFF0085FF.toInt()
+            map = naverMap
+        }
+    }
 }
