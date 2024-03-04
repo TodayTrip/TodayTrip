7294b44b741e45584f9c0ca101c580bf9f6ced67
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2024-03-05 04:31:00 +0900

    [Feat] placeDetail 진행중

--- app/src/main/java/com/twoday/todaytrip/ui/place_detail/PlaceDetailActivity.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/place_detail/PlaceDetailActivity.kt
@@ -1,9 +1,15 @@
 package com.twoday.todaytrip.ui.place_detail
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.twoday.todaytrip.MyApplication
+import com.twoday.todaytrip.R
 import com.twoday.todaytrip.databinding.ActivityPlaceDetailBinding
+import com.twoday.todaytrip.place_list_adapter.OnTourItemClickListener
+import com.twoday.todaytrip.tourData.TourItem
 
 class PlaceDetailActivity : AppCompatActivity() {
 
@@ -11,9 +17,12 @@ class PlaceDetailActivity : AppCompatActivity() {
 
     private val viewModel: PlaceDetailViewModel by viewModels()
 
-    private val placePhotoAdapter by lazy { PlaceDetailPhotoAdapter() }
+    //대표사진 한장만
+//    private val placePhotoAdapter by lazy { PlaceDetailPhotoAdapter() }
     private val placeInfoAdapter by lazy { PlaceDetailExtraInfoAdapter() }
     private val placeMemoryAdapter by lazy { PlaceDetailMyMemoryAdapter() }
+
+    //getExtra(장소 TourItem parcelable)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +32,43 @@ class PlaceDetailActivity : AppCompatActivity() {
     }
 
     private fun initView() {
+//        val introPhotoRecyclerView = binding.rvPlaceDetailPic
         val extraInfoRecyclerView = binding.rvPlaceDetailExtraInfoList
         val myMemoryRecyclerView = binding.rvPlaceDetailMyMemoryList
-
+//        introPhotoRecyclerView.adapter = placePhotoAdapter
+        extraInfoRecyclerView.adapter = placeInfoAdapter
+        myMemoryRecyclerView.adapter = placeMemoryAdapter
+        binding.ivPlaceDetailPic.setImageURI()
+        Glide.with(binding.ivPlaceDetailPic)
+            .load()
+            .into(binding.ivPlaceDetailPic)
     }
+
+    fun initOnClickListener(item: TourItem) {
+        binding.tvPlaceDetailAddPathBtn.setOnClickListener {
+            OnTourItemClickListener.onTourItemClick(item)
+            setAddButtonUI(item.isAdded)
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun setAddButtonUI(isAdded: Boolean) {
+        binding.tvPlaceDetailAddPathBtn.background =
+            MyApplication.appContext!!.resources.getDrawable(
+                if (isAdded) R.drawable.shape_white_with_border_radius_10
+                else R.drawable.shape_mainblue_10_radius
+            )
+        binding.tvPlaceDetailAddPathBtn.text = MyApplication.appContext!!.resources.getText(
+            if (isAdded) R.string.item_place_list_remove
+            else R.string.item_place_list_add
+        )
+        binding.tvPlaceDetailAddPathBtn.setTextColor(
+            MyApplication.appContext!!.resources.getColor(
+                if (isAdded) R.color.main_blue
+                else R.color.white
+            )
+        )
+    }
+
+
 }
--- app/src/main/java/com/twoday/todaytrip/ui/place_detail/PlaceDetailViewModel.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/place_detail/PlaceDetailViewModel.kt
@@ -1,14 +1,42 @@
 package com.twoday.todaytrip.ui.place_detail
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.twoday.todaytrip.tourApi.AreaBasedListItem
 import com.twoday.todaytrip.tourData.TourItem
+import com.twoday.todaytrip.viewModel.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class PlaceDetailViewModel: ViewModel() {
+
+class PlaceDetailViewModel(
+    private val tourItem: TourItem
+): ViewModel() {
+
+    private val currentDestination: String? = null
+    private val isAddedToRoute: Boolean = false
+
+    private val placeId: String? = null
 
     private var _placeItemData = MutableLiveData<TourItem>()
     val placeItemData: LiveData<TourItem> get() = _placeItemData
 
-    private var _placeMemory = MutableLiveData<>()
+    private var _placeMemory = MutableLiveData<List<String>>()
+    val placeMemory: LiveData<List<String>> get() = _placeMemory
+
+    init {
+        getPlaceInfo()
+    }
+
+    fun getPlaceInfo() {
+        tourItem.getThumbnailImage()
+        //glide-> 이미지뷰
+        tourItem.getTitle()
+        tourItem.getAddress()
+        tourItem.getDetailInfoWithLabel()
+        //리사이클러뷰 dataset
+    }
 }
