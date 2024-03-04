b192f19a308bebd0457c14e4135176aed7ce7107
sunjoolee <sheisthepoem@naver.com>
2024-03-04 18:42:52 +0900

    [Feat] Add Record data class

---
+++ app/src/main/java/com/twoday/todaytrip/recordData/Record.kt
@@ -0,0 +1,23 @@
+package com.twoday.todaytrip.recordData
+
+import com.twoday.todaytrip.MyApplication
+import com.twoday.todaytrip.tourData.TourItem
+import com.twoday.todaytrip.utils.DateTimeUtil
+import com.twoday.todaytrip.utils.PrefConstants
+import com.twoday.todaytrip.utils.SharedPreferencesUtil
+
+data class Record(
+    val route: List<Pair<TourItem, List<String>>> // Pair<장소, 장소에 추가한 사진 URL 목록>
+){
+    val destination:String
+    val travelDate:String
+    init{
+        destination = loadDestination()
+        travelDate = DateTimeUtil.getCurrentDate()
+    }
+    private fun loadDestination() =
+        SharedPreferencesUtil.loadDestination(
+            MyApplication.appContext!!,
+            PrefConstants.DESTINATION_KEY
+        )?:""
+}
