eedaeadee55fa1e737441fde1cc19b914d31769c
nhkim <nhkim1529@gmail.com>
2024-03-05 16:03:33 +0900

    [Feat] PlaceMapViewModel에 지도 관련 코드 추가

--- app/src/main/java/com/twoday/todaytrip/viewModel/PlaceMapViewModel.kt
+++ app/src/main/java/com/twoday/todaytrip/viewModel/PlaceMapViewModel.kt
@@ -4,11 +4,8 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.naver.maps.map.overlay.Marker
 import com.twoday.todaytrip.PlaceData
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.lang.Exception
 
 class PlaceMapViewModel : ViewModel() {
 
@@ -17,23 +14,37 @@ class PlaceMapViewModel : ViewModel() {
     val placeCardList: LiveData<List<PlaceData>>
         get() = _placeCardList
 
-    init {
-        updatePlaceList()
-    }
+    // 가장 먼 거리의 마커쌍을 저장하는 라이브 데이터
+    private val _furthestPair = MutableLiveData<Pair<Marker, Marker>?>()
+    val furthestPair: LiveData<Pair<Marker, Marker>?>
+        get() {
+            Log.d("furthestPair", "RecordDetailViewModel에서 furthestPair 라이브데이터")
+            return _furthestPair
+        }
 
     private fun getPlaceData(): List<PlaceData> {
         TODO("Not yet implemented")
     }
 
-    private fun updatePlaceList() = viewModelScope.launch(Dispatchers.IO) {
-        try {
-            val result = getPlaceData()
-//            _placeCardList.postValue(
-//                result.map {  }
-//            )
-
-        } catch (e: Exception) {
-            Log.e("PlaceMapViewModel", "place list error = ${e.message}", e)
+    // 지도에서 가장 멀리 있는 마커 구하는 함수
+    fun findFurthestMarkers(markers: List<Marker>) {
+        if (markers.size < 2) {
+            _furthestPair.value = null
+            return
         }
+
+        var furthestPair: Pair<Marker, Marker>? = null
+        var longestDistance = 0.0
+
+        markers.forEach { marker1 ->
+            markers.forEach { marker2 ->
+                val distance = marker1.position.distanceTo(marker2.position)
+                if (distance > longestDistance) {
+                    longestDistance = distance
+                    furthestPair = Pair(marker1, marker2)
+                }
+            }
+        }
+        _furthestPair.value = furthestPair
     }
 }
