c3ee6080f145ef1e83818dd77284b6eccdfd68fa
sunjoolee <sheisthepoem@naver.com>
2024-03-06 13:43:23 +0900

    [Feat] Save/Load Record in Shared Preference

--- app/src/main/java/com/twoday/todaytrip/recordData/Record.kt
+++ app/src/main/java/com/twoday/todaytrip/recordData/Record.kt
@@ -2,12 +2,15 @@ package com.twoday.todaytrip.recordData
 
 import com.twoday.todaytrip.MyApplication
 import com.twoday.todaytrip.tourData.TourItem
+import com.twoday.todaytrip.ui.route.SavePhotoData
 import com.twoday.todaytrip.utils.DateTimeUtil
 import com.twoday.todaytrip.utils.DestinationPrefUtil
 import com.twoday.todaytrip.utils.PrefConstants
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Record(
-    val route: List<Pair<TourItem, String>> // Pair<장소, 장소에 추가한 사진 URL>
+    val savePhotoDataList: List<SavePhotoData>
 ){
     val destination:String
     val travelDate:String
--- app/src/main/java/com/twoday/todaytrip/ui/route/SavePhotoData.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/route/SavePhotoData.kt
@@ -2,8 +2,10 @@ package com.twoday.todaytrip.ui.route
 
 import android.net.Uri
 import com.twoday.todaytrip.tourData.TourItem
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class SavePhotoData(
     val tourItem: TourItem,
     val imageUrl: String? = null
-)
+): java.io.Serializable
