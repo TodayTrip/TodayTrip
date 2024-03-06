cf73585a0b51bad12b127eca67ee44e47c1107c0
sunjoolee <sheisthepoem@naver.com>
2024-03-06 13:46:08 +0900

    [Refactor] Refactor code, Submit new dataset on resume

--- app/src/main/java/com/twoday/todaytrip/ui/route/RouteFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/route/RouteFragment.kt
@@ -5,18 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.util.FusedLocationSource
-import com.twoday.todaytrip.R
 import com.twoday.todaytrip.databinding.FragmentRouteBinding
 import com.twoday.todaytrip.utils.ContentIdPrefUtil
 import com.twoday.todaytrip.utils.TourItemPrefUtil
-
 
 class RouteFragment : Fragment(), OnMapReadyCallback {
 
@@ -35,11 +32,6 @@ class RouteFragment : Fragment(), OnMapReadyCallback {
     private lateinit var map: NaverMap
     private lateinit var locationSource: FusedLocationSource
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -52,28 +44,7 @@ class RouteFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val contentIdList = ContentIdPrefUtil.loadContentIdList()
-        val tour = TourItemPrefUtil.loadTouristAttractionList()
-        val rest = TourItemPrefUtil.loadRestaurantList()
-        val cafe = TourItemPrefUtil.loadCafeList()
-        val event = TourItemPrefUtil.loadEventList()
-        val tourLIst = tour + rest + cafe + event
-        val dataSet:MutableList<RouteListData> = mutableListOf()
-
-        //순서 변경하면 변경사항 저장되게하기
-        //담은 아이템 RecyclerView에 띄우기
-        if(contentIdList.isNotEmpty()) {
-            var count = 0
-            contentIdList.forEach {
-                val place = tourLIst.find { it.getContentId() == contentIdList[count] }
-                Log.d("sdc","최초카운트 = $count")
-                dataSet.add(RouteListData(place?.getTitle() ?: "", place?.getAddress() ?: ""))
-                if (contentIdList.size-1 > count) {
-                    count += 1
-                }
-            }
-        }
-
+        val dataSet = initDataSet()
         adapter.submitList(dataSet)
         binding.rvRouteRecyclerview.adapter = adapter
         //리싸이클러뷰에 아무것도 없을시 아이콘 띄움
@@ -123,15 +94,28 @@ class RouteFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RouteFragment().apply {
-                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
+    override fun onResume() {
+        adapter.submitList(initDataSet())
+        super.onResume()
+    }
+
+    private fun initDataSet():List<RouteListData>{
+        val contentIdList = ContentIdPrefUtil.loadContentIdList()
+        val tourList = TourItemPrefUtil.loadAllTourItemList()
+        val dataSet:MutableList<RouteListData> = mutableListOf()
+
+        if(contentIdList.isNotEmpty()) {
+            var count = 0
+            contentIdList.forEach {
+                val place = tourList.find { it.getContentId() == contentIdList[count] }
+                Log.d("sdc","최초카운트 = $count")
+                dataSet.add(RouteListData(place?.getTitle() ?: "", place?.getAddress() ?: ""))
+                if (contentIdList.size-1 > count) {
+                    count += 1
                 }
             }
+        }
+        return dataSet
     }
 
     override fun onMapReady(naverMap: NaverMap) {
