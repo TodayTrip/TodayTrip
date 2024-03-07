0bd4744edeadf460f8c92253c87b510fc6ff1ed2
nhkim <nhkim1529@gmail.com>
2024-03-07 11:59:18 +0900

    [Comment] 주석 삭제

--- app/src/main/java/com/twoday/todaytrip/ui/record/RecordFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/record/RecordFragment.kt
@@ -1,7 +1,6 @@
 package com.twoday.todaytrip.ui.record
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,19 +9,13 @@ import androidx.core.view.isVisible
 import com.db.williamchart.data.AxisType
 import com.twoday.todaytrip.R
 import com.twoday.todaytrip.databinding.FragmentRecordBinding
-import com.twoday.todaytrip.utils.RecordPrefUtil
 
 class RecordFragment : Fragment() {
-    private val TAG = "RecordActivity"
     private var _binding: FragmentRecordBinding? = null
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    val recordList = RecordPrefUtil.loadRecordList()
-//        Log.d(TAG, "${recordList.size}")
-//        if(recordList.isNotEmpty())
-//            Log.d(TAG, "${recordList[0].destination}, ${recordList[0].travelDate}, ${recordList[0].savePhotoDataList.size}")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,12 +31,6 @@ class RecordFragment : Fragment() {
     }
 
     private fun initView() {
-        // Shared Preference test code
-        val recordList = RecordPrefUtil.loadRecordList()
-        Log.d(TAG, "${recordList.size}")
-        if(recordList.isNotEmpty())
-            Log.d(TAG, "${recordList[0].destination}, ${recordList[0].travelDate}, ${recordList[0].savePhotoDataList.size}")
-
         //WilliamsChart example code
         binding.tvChartEmpty.isVisible = false
         binding.chartRecord.isVisible = true
@@ -51,9 +38,10 @@ class RecordFragment : Fragment() {
         val exampleDataSet = listOf<Pair<String, Float>>(
             "부산" to 10F,
             "영월" to 6F,
-            "강원도 외 3" to 1F)
+            "강원도 외 3" to 1F
+        )
             .sortedBy { it.second }
-        binding.chartRecord.run{
+        binding.chartRecord.run {
             axis = AxisType.Y
             labelsSize = 30F
             labelsColor = resources.getColor(R.color.middle_gray)
