62a8aa4aa7068fa4cf35f7a846046068572de83a
sunjoolee <sheisthepoem@naver.com>
2024-03-09 05:03:32 +0900

    [Feat] Feat Back button pressed in RecordDetailActivity

--- app/src/main/java/com/twoday/todaytrip/ui/record_detail/RecordDetailActivity.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/record_detail/RecordDetailActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.lifecycle.Observer
@@ -14,6 +16,8 @@ import com.twoday.todaytrip.viewModel.RecordDetailViewModel
 import com.twoday.todaytrip.ui.record.Record
 
 class RecordDetailActivity : AppCompatActivity() {
+    private val TAG = "RecordDetailActivity"
+
     private var _binding: ActivityRecordDetailBinding? = null
     private val binding get() = _binding!!
 
@@ -22,6 +26,13 @@ class RecordDetailActivity : AppCompatActivity() {
     }
     private val record:Record? by lazy{
         intent.getParcelableExtra(EXTRA_RECORD)
+    }
+
+    private val onBackPressedCallback = object:OnBackPressedCallback(true){
+        override fun handleOnBackPressed() {
+            Log.d(TAG, "handleOnBackPressed) called")
+            finish()
+        }
     }
 
     companion object{
@@ -37,6 +48,7 @@ class RecordDetailActivity : AppCompatActivity() {
         _binding = ActivityRecordDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        this.onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
         initModelObserver()
 
         initUI()
@@ -74,7 +86,10 @@ class RecordDetailActivity : AppCompatActivity() {
         }
     }
     private fun initBackButton(){
-        // TODO
+        binding.ivRecordDetailBack.setOnClickListener {
+            finish()
+        }
+
     }
     private fun initDeleteButton(){
         // TODO
