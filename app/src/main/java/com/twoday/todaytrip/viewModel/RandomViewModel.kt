cf90ee438770c1400bd3fefa4d6f1211c325594b
nhkim <nhkim1529@gmail.com>
2024-03-12 13:00:05 +0900

    [Feat] RandomOptionFragment에 들어가는 비즈니스 로직 RandomViewModel로 분리

--- app/src/main/java/com/twoday/todaytrip/viewModel/RandomViewModel.kt
+++ app/src/main/java/com/twoday/todaytrip/viewModel/RandomViewModel.kt
@@ -1,32 +1,12 @@
 package com.twoday.todaytrip.viewModel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.twoday.todaytrip.utils.ContentIdPrefUtil
 import com.twoday.todaytrip.utils.DestinationData
 import com.twoday.todaytrip.utils.DestinationPrefUtil
+import com.twoday.todaytrip.utils.TourItemPrefUtil
 
-class RandomViewModel: ViewModel() {
-
-    private val _loginStatus = MutableLiveData<LoginResult>()
-    val loginStatus: LiveData<LoginResult> = _loginStatus
-
-    // FirebaseAuth 인스턴스를 변수로 선언
-    private var auth: FirebaseAuth = Firebase.auth
-
-    //익명 로그인 함수
-    fun performAnonymousLogin() {
-        auth.signInAnonymously().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                _loginStatus.value = LoginResult.SUCCESS
-            } else {
-                _loginStatus.value = LoginResult.FAILURE
-            }
-        }
-    }
+class RandomViewModel : ViewModel() {
 
     // 전체 랜덤 시 여행지 랜덤 선택하는 함수, 테마 Sharf에는 null로 저장
     fun selectRandomDestination() {
@@ -34,9 +14,9 @@ class RandomViewModel: ViewModel() {
         DestinationPrefUtil.saveDestination(randomDestination)
         DestinationPrefUtil.saveTheme("")
     }
-}
 
-enum class LoginResult {
-    SUCCESS,
-    FAILURE
+    fun resetSharedPref() {
+        TourItemPrefUtil.resetTourItemListPref()
+        ContentIdPrefUtil.resetContentIdListPref()
+    }
 }
