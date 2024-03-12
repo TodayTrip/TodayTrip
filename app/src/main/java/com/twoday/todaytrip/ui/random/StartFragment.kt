69ff4f59b7969b4f4d1813c69a4509209c6db39a
nhkim <nhkim1529@gmail.com>
2024-03-12 14:01:17 +0900

    [Fix] 익명 로그인 기능 없애고 viewModel에 비즈니스 로직 분리

--- app/src/main/java/com/twoday/todaytrip/ui/random/StartFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/random/StartFragment.kt
@@ -5,16 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.twoday.todaytrip.R
 import com.twoday.todaytrip.databinding.FragmentStartBinding
-import com.twoday.todaytrip.utils.ContentIdPrefUtil
-import com.twoday.todaytrip.utils.TourItemPrefUtil
-import com.twoday.todaytrip.utils.showSnackBar
-import com.twoday.todaytrip.viewModel.LoginResult
 import com.twoday.todaytrip.viewModel.RandomViewModel
 
 class StartFragment : Fragment() {
@@ -31,23 +26,12 @@ class StartFragment : Fragment() {
     ): View? {
         _binding = FragmentStartBinding.inflate(inflater, container, false)
         return binding.root
-//        setGoogleLogin()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
-        initModelObserver()
         setUpClickListener()
-    }
-
-    private fun initModelObserver() {
-        viewModel.loginStatus.observe(viewLifecycleOwner, Observer { result ->
-            when (result) {
-                LoginResult.SUCCESS -> findNavController().navigate(R.id.action_navigation_start_to_navigation_random_option)
-                LoginResult.FAILURE -> showSnackBar(R.string.login_fail)
-            }
-        })
     }
 
     private fun initView() {
@@ -58,15 +42,9 @@ class StartFragment : Fragment() {
 
     private fun setUpClickListener() {
         binding.btnStartTrip.setOnClickListener {
-            viewModel.performAnonymousLogin()
-            resetSharedPref()
+            viewModel.resetSharedPref()
+            findNavController().navigate(R.id.action_navigation_start_to_navigation_random_option)
         }
-    }
-
-    //TODO 이 함수도 RandomViewModel로 빼놓으면 어떨까요?
-    private fun resetSharedPref() {
-        TourItemPrefUtil.resetTourItemListPref()
-        ContentIdPrefUtil.resetContentIdListPref()
     }
 
     override fun onDestroyView() {
@@ -74,17 +52,3 @@ class StartFragment : Fragment() {
         _binding = null
     }
 }
-
-// 구글 로그인 함수(필요할까?)
-//    fun setGoogleLogin(){
-//        // 요청 정보 옵션
-//        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestIdToken(getString(R.string.default_web_client_id))
-//            .requestEmail().build()
-//        client = GoogleSignIn.getClient(requireContext(), options)
-//
-//        binding.loginButton.setOnClickListener {
-//            // 로그인 요청
-//            startActivityForResult(client.signInIntent, 1)
-//        }
-//    }
