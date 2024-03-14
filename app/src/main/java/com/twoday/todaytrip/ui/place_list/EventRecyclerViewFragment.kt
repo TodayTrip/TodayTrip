56c8ee0dd4ddf959442041f8dd9568ea7cff4287
sunjoolee <sheisthepoem@naver.com>
2024-03-15 00:31:52 +0900

    [Feat] 행사/축제 탭 스와이프 새로고침 & 무한 스크롤

--- app/src/main/java/com/twoday/todaytrip/ui/place_list/EventRecyclerViewFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/place_list/EventRecyclerViewFragment.kt
@@ -13,11 +13,14 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.twoday.todaytrip.R
 import com.twoday.todaytrip.databinding.FragmentPlaceListEventRecyclerViewBinding
-import com.twoday.todaytrip.ui.place_list.adapter.OnTourItemClickListener
-import com.twoday.todaytrip.ui.place_list.adapter.PlaceListAdapter
 import com.twoday.todaytrip.tourData.TourItem
 import com.twoday.todaytrip.ui.place_detail.PlaceDetailActivity
+import com.twoday.todaytrip.ui.place_list.adapter.OnTourItemClickListener
+import com.twoday.todaytrip.ui.place_list.adapter.PlaceListAdapter
 import com.twoday.todaytrip.viewModel.MainViewModel
 
 
@@ -34,49 +37,107 @@ class EventRecyclerViewFragment : Fragment(), OnTourItemClickListener {
         }
     }
 
-    private lateinit var adapter: PlaceListAdapter
+    private lateinit var eventAdapter: PlaceListAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        _binding = FragmentPlaceListEventRecyclerViewBinding.inflate(layoutInflater, container, false)
+        _binding =
+            FragmentPlaceListEventRecyclerViewBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setLoadingUI(true)
+        initUI()
+        initSwipeRefreshLayout()
         initRecyclerView()
         initModelObserver()
     }
-    private fun setLoadingUI(isLoading: Boolean) {
-        binding.shimmerEventRecyclerView.isVisible = isLoading
-        binding.rvEventRecyclerView.isVisible = !isLoading
 
-        if(isLoading) binding.shimmerEventRecyclerView.startShimmer()
+    private fun initUI() {
+        setLoadingUI(true)
+        setNoResultUI(false)
+
+        Glide.with(requireContext())
+            .load(resources.getDrawable(R.drawable.gif_place_list_no_result))
+            .into(binding.ivEventRecyclerViewNoResult)
     }
-    private fun initRecyclerView(){
-        adapter = PlaceListAdapter().apply{
+
+    private fun setNoResultUI(isNoResult: Boolean) {
+        Log.d(TAG, "setNoResultUI) isNoResult: $isNoResult")
+        binding.layoutEventRecyclerViewNoResult.isVisible = isNoResult
+    }
+
+    private fun setLoadingUI(isLoading: Boolean) {
+        Log.d(TAG, "setLoadingUI) isLoading: $isLoading")
+        binding.shimmerEventRecyclerView.isVisible = isLoading
+        if (isLoading) binding.shimmerEventRecyclerView.startShimmer()
+    }
+
+    private fun initSwipeRefreshLayout() {
+        binding.swipeEventRecyclerView.setOnRefreshListener {
+            setNoResultUI(false)
+            setLoadingUI(true)
+            mainModel.loadOrFetchEventList()
+
+            binding.swipeEventRecyclerView.isRefreshing = false
+        }
+    }
+
+    private fun initRecyclerView() {
+        eventAdapter = PlaceListAdapter().apply {
             onTourItemClickListener = this@EventRecyclerViewFragment
         }
-        binding.rvEventRecyclerView.adapter = adapter
+        binding.rvEventRecyclerView.run {
+            this.adapter = eventAdapter
+            initScrollListener(this)
+        }
     }
+
+    private fun initScrollListener(recyclerView: RecyclerView) {
+        recyclerView.setOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    Log.d(TAG, "recyclerview end of scroll!")
+                    Log.d(TAG, "adapter current list size: ${eventAdapter.currentList.size}")
+                    Log.d(TAG, "isEventLoadReady: ${mainModel.isEventLoadReady}")
+
+                    if ((eventAdapter.currentList.isNotEmpty()) &&
+                        (mainModel.isEventLoadReady)
+                    ) {
+                        Log.d(TAG, "fetch and save more event list")
+                        eventAdapter.addDummyTourItem()
+                        mainModel.fetchAndSaveMoreEventList()
+                    }
+                }
+            }
+        })
+    }
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onTourItemClick(tourItem: TourItem) {
         Log.d(TAG, "onTourItemClick) called, ${tourItem.getTitle()}")
         val placeDetailIntent = PlaceDetailActivity.newIntent(
             requireContext(),
             tourItem.getContentTypeId(),
-            tourItem)
+            tourItem
+        )
         startActivity(placeDetailIntent)
     }
 
-    private fun initModelObserver(){
-        mainModel.eventList.observe(viewLifecycleOwner, Observer {
-            Log.d(TAG, "event list size: ${it.size}")
-            adapter.submitList(it.toMutableList())
+    private fun initModelObserver() {
+        mainModel.eventList.observe(viewLifecycleOwner, Observer {eventList ->
+            Log.d(TAG, "observe) event list size: ${eventList.size}")
+            eventAdapter.submitList(eventList.toMutableList())
+            Log.d(TAG, "observe) current list size: ${eventAdapter.currentList.size}")
+
             setLoadingUI(false)
+            if (eventList.isEmpty()) setNoResultUI(true)
+
         })
     }
 
