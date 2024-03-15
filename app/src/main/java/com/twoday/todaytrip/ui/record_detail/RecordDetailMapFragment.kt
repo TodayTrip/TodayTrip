7befeb371dabb8202656d8b783a5efa1840b5349
nhkim <nhkim1529@gmail.com>
2024-03-15 19:51:54 +0900

    [Design] 마커 이미지 변경

--- app/src/main/java/com/twoday/todaytrip/ui/record_detail/RecordDetailMapFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/record_detail/RecordDetailMapFragment.kt
@@ -97,7 +97,7 @@ class RecordDetailMapFragment : Fragment(), OnMapReadyCallback {
         this.naverMap = naverMap
 
         val markerIconBitmap =
-            resizeMapIcons(requireContext(), R.drawable.img_pic_marker, 120, 120)
+            resizeMapIcons(requireContext(), R.drawable.img_pic_marker, 100, 100)
         val combinedBitmap = combineImages(markerIconBitmap, markerIconBitmap)
 
         locations.forEach { latLng ->
