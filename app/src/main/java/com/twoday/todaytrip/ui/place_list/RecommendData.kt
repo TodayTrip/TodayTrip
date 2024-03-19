900a9aa7e64396e6984bf8466eb750d72628e05f
sunjoolee <sheisthepoem@naver.com>
2024-03-19 16:57:22 +0900

    [Feat] Change RecommendData data class to interface
    Feat RecommendCover data class
    Feat RecommendTourItem data class

--- app/src/main/java/com/twoday/todaytrip/ui/place_list/RecommendData.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/place_list/RecommendData.kt
@@ -2,9 +2,28 @@ package com.twoday.todaytrip.ui.place_list
 
 import com.twoday.todaytrip.tourData.TourItem
 
-data class RecommendData(
-    val imageUri: String,
-    val subTitle: String,
+enum class RecommendDataViewType(val viewType: Int){
+    COVER(0),
+    TOUR_ITEM(1)
+}
+interface RecommendData{
+    fun getViewType():Int
+}
+data class RecommendCover(
+    val viewType: Int = RecommendDataViewType.COVER.viewType,
+    val imageId: Int,
+    val subTitleId: Int,
+    val title: String
+): RecommendData{
+    override fun getViewType(): Int = viewType
+}
+
+data class RecommendTourItem(
+    val viewType: Int = RecommendDataViewType.TOUR_ITEM.viewType,
+    val imageUrl: String,
+    val subTitleId: Int,
     val title: String,
-    val tourItem: TourItem? = null
-)
+    val tourItem: TourItem
+): RecommendData{
+    override fun getViewType(): Int = viewType
+}
