e9fb14f3a609eff9475e4f6a760a512045b1700a
nhkim <nhkim1529@gmail.com>
2024-03-21 02:14:34 +0900

    [Comment] 주석 삭제

--- app/src/main/java/com/twoday/todaytrip/ui/random/StartFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/random/StartFragment.kt
@@ -45,10 +45,6 @@ class StartFragment : Fragment() {
             viewModel.resetSharedPref()
             findNavController().navigate(R.id.action_navigation_start_to_navigation_random_option)
         }
-//        binding.btnSelectRegion.setOnClickListener {
-//            viewModel.resetSharedPref()
-//            findNavController().navigate(R.id.action_navigation_start_to_navigation_select_region)
-//        }
     }
 
     override fun onDestroyView() {
