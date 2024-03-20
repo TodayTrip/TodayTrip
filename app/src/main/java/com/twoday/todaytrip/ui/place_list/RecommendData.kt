0f951049243bacabae6fd7bde872f54e6f89cdb6
sunjoolee <sheisthepoem@naver.com>
2024-03-20 12:58:19 +0900

    [Refactor] Set default value of RecommendCover constructor properties

--- app/src/main/java/com/twoday/todaytrip/ui/place_list/RecommendData.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/place_list/RecommendData.kt
@@ -1,12 +1,15 @@
 package com.twoday.todaytrip.ui.place_list
 
+import com.twoday.todaytrip.R
 import com.twoday.todaytrip.tourData.TourItem
 
 sealed interface RecommendData{}
 data class RecommendCover(
     val imageId: Int,
-    val subTitleId: Int,
-    val title: String
+    val subTitleId: Int = R.string.place_list_recommend_sub_title_cover,
+    val titleId: Int = R.string.place_list_recommend_title_cover,
+    val destination: String,
+    val destinationSigungu: String
 ): RecommendData
 
 data class RecommendTourItem(
