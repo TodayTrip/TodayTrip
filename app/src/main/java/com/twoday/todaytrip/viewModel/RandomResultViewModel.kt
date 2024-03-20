4bb3ca55da7bc49ae26165dabfa48c5ac2d4f4c4
sunjoolee <sheisthepoem@naver.com>
2024-03-20 22:21:49 +0900

    [Feat] Clear Shared Preference files
    Clear Shared Preference files when new destination is selected

--- app/src/main/java/com/twoday/todaytrip/viewModel/RandomResultViewModel.kt
+++ app/src/main/java/com/twoday/todaytrip/viewModel/RandomResultViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.twoday.todaytrip.tourApi.TourNetworkInterfaceUtils
+import com.twoday.todaytrip.utils.ContentIdPrefUtil
 import com.twoday.todaytrip.utils.DestinationData
 import com.twoday.todaytrip.utils.DestinationPrefUtil
 import com.twoday.todaytrip.utils.PageNoPrefUtil
+import com.twoday.todaytrip.utils.RecommendPrefUtil
 import com.twoday.todaytrip.utils.TourItemPrefUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -31,11 +33,21 @@ class RandomResultViewModel : ViewModel() {
         get() = _isTouristAttractionListReady
 
     init {
+        resetSharedPref()
+
         _isTouristAttractionListReady.value = false
         CoroutineScope(Dispatchers.IO).launch {
             fetchAndSaveTourItemList()
         }
     }
+
+    private fun resetSharedPref() {
+        TourItemPrefUtil.resetTourItemListPref() // 관광지 정보 목록 초기화
+        ContentIdPrefUtil.resetContentIdListPref() // 경로에 담은 관광지 정보 초기화
+        PageNoPrefUtil.resetPageNoPref() // 관광지 목록 별 pageNo 초기화
+        RecommendPrefUtil.resetRecommendTourItemPref() // 여행지 추천 코스 초기화
+    }
+
 
     private suspend fun fetchAndSaveTourItemList() {
         val touristAttractionJob = CoroutineScope(Dispatchers.IO).launch {
