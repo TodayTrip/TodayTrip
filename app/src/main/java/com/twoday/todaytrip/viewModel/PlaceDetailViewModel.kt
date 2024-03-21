f7546d46d488c66277adf098b7bc5e05ee44c8c7
sunjoolee <sheisthepoem@naver.com>
2024-03-22 00:53:10 +0900

    [Feat] 근처 관광지 정보 Shared Preference에 저장

--- app/src/main/java/com/twoday/todaytrip/viewModel/PlaceDetailViewModel.kt
+++ app/src/main/java/com/twoday/todaytrip/viewModel/PlaceDetailViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.twoday.todaytrip.tourApi.TourNetworkInterfaceUtils
+import com.twoday.todaytrip.tourData.TourCategoryId3
 import com.twoday.todaytrip.tourData.TourItem
 import com.twoday.todaytrip.ui.place_detail.MemoryData
 import com.twoday.todaytrip.ui.place_list.adapter.OnTourItemAddClickListener
 import com.twoday.todaytrip.utils.ContentIdPrefUtil
 import com.twoday.todaytrip.utils.RecordPrefUtil
+import com.twoday.todaytrip.utils.TourItemPrefUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -63,6 +65,26 @@ class PlaceDetailViewModel() : ViewModel() {
             withContext(Dispatchers.Main){
                 Log.d(TAG, "initNearbyList) fetchedNearByList: ${fetchedNearByList.toString()}")
                 _nearByList.value = fetchedNearByList
+                saveNearByList()
+            }
+        }
+    }
+    private fun saveNearByList(){
+        _nearByList.value?.forEach { tourItem ->
+            when(tourItem){
+                is TourItem.Restaurant ->{
+                    if((!tourItem.tourItemInfo.category3.isNullOrEmpty()) &&
+                        (tourItem.tourItemInfo.category3 == TourCategoryId3.CAFE_AND_TEA.id))
+                        TourItemPrefUtil.saveMoreCafeList(listOf(tourItem))
+                    else
+                        TourItemPrefUtil.saveMoreRestaurantList(listOf(tourItem))
+                }
+                is TourItem.EventPerformanceFestival -> {
+                    TourItemPrefUtil.saveMoreEventList(listOf(tourItem))
+                }
+                else ->{ // TouristDestination, CulturalFacilities, LeisureSports
+                    TourItemPrefUtil.saveMoreTouristAttractionList(listOf(tourItem))
+                }
             }
         }
     }
