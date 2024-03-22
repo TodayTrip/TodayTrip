a51836b639b697c5cdbf9611f7b7e939ad2d51ca
sunjoolee <sheisthepoem@naver.com>
2024-03-22 19:04:31 +0900

    [Feat] 정보탭 여행지 타이틀 테마 추가 완료

--- app/src/main/java/com/twoday/todaytrip/ui/place_list/PlaceListFragment.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/place_list/PlaceListFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -143,6 +144,18 @@ class PlaceListFragment : Fragment(),
     }
 
     private fun initModelObserver() {
+        model.themeTitleInfo.observe(viewLifecycleOwner){themeTitleInfo ->
+            if(themeTitleInfo.first == -1) {
+                binding.tvTravelTheme.isVisible = false
+            }
+            else {
+                binding.tvTravelTheme.run{
+                    isVisible = true
+                    setText(themeTitleInfo.first)
+                    setTextColor(resources.getColor(themeTitleInfo.second))
+                }
+            }
+        }
         model.destination.observe(viewLifecycleOwner) { destination ->
             binding.tvTravelDestination.text = destination
         }
--- app/src/main/java/com/twoday/todaytrip/viewModel/PlaceListViewModel.kt
+++ app/src/main/java/com/twoday/todaytrip/viewModel/PlaceListViewModel.kt
@@ -36,8 +36,8 @@ class PlaceListViewModel : ViewModel() {
     private val _destination = MutableLiveData<String>()
     val destination: LiveData<String> get() = _destination
 
-    private val _themeTitle = MutableLiveData<String>()
-    val themeTitle: LiveData<String> get() = _themeTitle
+    private val _themeTitleInfo = MutableLiveData<Pair<Int,Int>>()
+    val themeTitleInfo: LiveData<Pair<Int, Int>> get() = _themeTitleInfo
 
     private val _weatherInfo = MutableLiveData<WeatherInfo>()
     val weatherInfo: LiveData<WeatherInfo> get() = _weatherInfo
@@ -57,6 +57,7 @@ class PlaceListViewModel : ViewModel() {
     private val RECOMMEND_INDEX_EVENT = 4
 
     init {
+        initThemeTitleInfo()
         initDestination()
         initWeatherInfo()
         initRecommendDataList()
@@ -65,9 +66,18 @@ class PlaceListViewModel : ViewModel() {
     private fun initDestination() {
         _destination.value = DestinationPrefUtil.loadDestination()!!
     }
-    private fun initThemeTitle(){
+    private fun initThemeTitleInfo(){
         val loadedTheme = DestinationPrefUtil.loadTheme()
-        // TODO 테마 별 타이틀 문자열 반환
+        _themeTitleInfo.value = when(loadedTheme){
+            "산" -> R.string.theme_title_first to R.color.theme_title1
+            "바다" -> R.string.theme_title_second to R.color.theme_title2
+            "역사" -> R.string.theme_title_third to R.color.theme_title3
+            "휴양" -> R.string.theme_title_fourth to R.color.theme_title4
+            "체험" -> R.string.theme_title_fifth to R.color.theme_title5
+            "레포츠" -> R.string.theme_title_sixth to R.color.theme_title6
+            "문화시설" -> R.string.theme_title_seventh to R.color.theme_title7
+            else -> -1 to -1
+        }
     }
 
     private fun initWeatherInfo() {
