24b2129115b5cb12df251003de1b4a487557d4a5
parkjaewon <wodnjs3336@naver.com>
2024-03-25 15:45:23 +0900

    [Feat] 경로, 기록 삭제 후 토스트메시지

--- app/src/main/java/com/twoday/todaytrip/ui/route/RouteAdapter.kt
+++ app/src/main/java/com/twoday/todaytrip/ui/route/RouteAdapter.kt
@@ -11,7 +11,7 @@ import com.twoday.todaytrip.databinding.ItemRouteListBinding
 
 class RouteAdapter :
     ListAdapter<RouteListData, RouteAdapter.ViewHolder>(RouteListDataDiffCallback) {
-        private val TAG = "RouteAdapter"
+    private val TAG = "RouteAdapter"
 
     var onRouteListDataClickListener: OnRouteListDataClickListener? = null
     private var editMode: Boolean = false
@@ -25,7 +25,7 @@ class RouteAdapter :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentItem = getItem(position)
         Log.d(TAG, "onBindViewHolder) item.title: ${currentItem.name}, position: ${position}")
-        holder.position.text = (position+1).toString()
+        holder.position.text = (position + 1).toString()
         holder.icon.visibility = if (editMode) View.VISIBLE else View.GONE
         holder.view.visibility = if (editMode) View.INVISIBLE else View.VISIBLE
         holder.position.visibility = if (editMode) View.INVISIBLE else View.VISIBLE
@@ -34,23 +34,23 @@ class RouteAdapter :
 
         holder.icon.setOnClickListener {
             onRouteListDataClickListener?.onRouteListDataRemove(currentItem, position)
-            Toast.makeText(it.context,"경로가 삭제되었습니다.",Toast.LENGTH_SHORT).show()
+            Toast.makeText(it.context, "경로가 삭제되었습니다.", Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
-        if(payloads.isEmpty()) super.onBindViewHolder(holder, position, payloads)
-        else{
-            holder.position.text = (position+1).toString()
+        if (payloads.isEmpty()) super.onBindViewHolder(holder, position, payloads)
+        else {
+            holder.position.text = (position + 1).toString()
         }
     }
 
-    fun iconTogle(show: Boolean){
+    fun iconTogle(show: Boolean) {
         if (editMode != show) {
             editMode = show
             notifyDataSetChanged() // 모든 아이템을 다시 바인딩하도록 함
         }
-        Log.d("sdc","adapter $editMode")
+        Log.d("sdc", "adapter $editMode")
 //        submitList()
     }
 
@@ -63,17 +63,18 @@ class RouteAdapter :
         val icon = binding.ivRouteRemove
 
         fun initListener(item: RouteListData) {
-            itemView.setOnClickListener{
+            itemView.setOnClickListener {
                 Log.d(TAG, "RouteAdapter)setOnClickListener ${item.contentId}")
                 onRouteListDataClickListener?.onRouteListDataClick(item.contentId)
             }
         }
+
         fun bind(item: RouteListData) {
             name.text = item.name
             address.text = item.address
         }
 
-        fun iconTogleEdit(show: Boolean){
+        fun iconTogleEdit(show: Boolean) {
 //            getItem(0).remove=true
             if (editMode != show) {
                 editMode = !show
