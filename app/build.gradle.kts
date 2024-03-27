580e8c2542e619c451d2754df649837e4396f2fa
sunjoolee <sheisthepoem@naver.com>
2024-03-27 20:48:38 +0900

    [Setting] increment versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.twoday.todaytrip"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
