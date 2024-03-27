47b52847edf1ef609cfc8906bf4033c586b88b54
nhkim <nhkim1529@gmail.com>
2024-03-27 11:58:41 +0900

    [Feat] Glide 확장 함수 추가

--- app/src/main/java/com/twoday/todaytrip/utils/GlideUtil.kt
+++ app/src/main/java/com/twoday/todaytrip/utils/GlideUtil.kt
@@ -3,6 +3,7 @@ package com.twoday.todaytrip.utils
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
+import com.twoday.todaytrip.R
 
 fun ImageView.glide(
     url: String
@@ -18,5 +19,24 @@ fun ImageView.glide(
 ){
     Glide.with(this.context)
         .load(resId)
+        .into(this)
+}
+
+fun ImageView.glideWithPlaceholder(
+    @DrawableRes
+    resId: Int
+){
+    Glide.with(this.context)
+        .load(resId)
+        .placeholder(R.drawable.img_default)
+        .into(this)
+}
+
+fun ImageView.glideWithPlaceholder(
+    uri: String
+){
+    Glide.with(this.context)
+        .load(uri)
+        .placeholder(R.drawable.img_default)
         .into(this)
 }
