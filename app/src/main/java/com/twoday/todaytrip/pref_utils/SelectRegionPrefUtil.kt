58a68f580c9c074f7b72f4d95c015812400d5278
sunjoolee <sheisthepoem@naver.com>
2024-04-12 00:50:57 +0900

    [Refactor] Convert Gson to Moshi in SelectRegionPrefUtil

--- app/src/main/java/com/twoday/todaytrip/pref_utils/SelectRegionPrefUtil.kt
+++ app/src/main/java/com/twoday/todaytrip/pref_utils/SelectRegionPrefUtil.kt
@@ -3,12 +3,17 @@ package com.twoday.todaytrip.pref_utils
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.twoday.todaytrip.MyApplication
 
 object SelectRegionPrefUtil {
     private val TAG = "SelectRegionPrefUtil"
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
     fun loadSelectRegionList() = loadSelectRegionList(PrefConstants.SELECT_REGION_LIST_KEY)
     fun saveSelectRegionList(selectRegionList: MutableList<String>) = saveSelectRegionList(
@@ -29,7 +34,8 @@ object SelectRegionPrefUtil {
 
     private fun saveSelectRegionList(selectRegionList: List<String>, destinationKey: String) {
         val prefs = getSelectRegionListPreferences()
-        val json = Gson().toJson(selectRegionList)
+        val type = Types.newParameterizedType(List::class.java, String::class.java)
+        val json = moshi.adapter<List<String>>(type).toJson(selectRegionList)
         prefs.edit().putString(destinationKey, json).apply()
     }
 
@@ -37,8 +43,8 @@ object SelectRegionPrefUtil {
         val prefs = getSelectRegionListPreferences()
         val json = prefs.getString(destinationKey, null)
         return if ((json != null) && (json.toString() != "[]")) {
-            val type = object : TypeToken<List<String>>() {}.type
-            Gson().fromJson(json, type)
+            val type = Types.newParameterizedType(List::class.java, String::class.java)
+            moshi.adapter<List<String>>(type).fromJson(json)?.toMutableList() ?: mutableListOf()
         } else {
             mutableListOf()
         }
